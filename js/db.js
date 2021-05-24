be9262c552db43101e31283d576570e144900b2f
Nicolas Rosenberg <nrrosenberg@gmail.com>
2021-05-23 21:03:04 -0400

    initial commit

---
+++ js/db.js
@@ -0,0 +1,47 @@
+// Offline data
+
+db.enablePersistence()
+    .catch(err => {
+        if (err.code == 'failed-precondition'){
+            // Multiple tabs open
+            console.log('persistence failed')
+        } else if (err.code = 'unimplemented') {
+            console.log('persistance is not available')
+        }
+
+    })
+
+db.collection('posts').onSnapshot((snapshot) => {
+    console.log(snapshot.docChanges())
+    snapshot.docChanges().forEach(change => {
+        console.log(change.type ,change.doc.data())
+
+        if (change.type === 'added') {
+            // Add new post
+            renderPost(change.doc.data(), change.doc.id);
+        }
+        if (change.type === 'removed') {
+            //Remove post
+        }
+    });
+})
+
+// add new post
+
+const form = document.querySelector('form') 
+const forms = document.querySelectorAll('.side-form');
+  
+form.addEventListener('submit' , e => {
+    e.preventDefault()
+    const post = {
+        user: form['user'].value,
+        caption: form['caption'].value
+    }
+
+    db.collection('posts').add(post)
+        .catch(err => console.log(err))
+
+        form['user'].value = ''
+        form['caption'].value = ''
+        M.Sidenav.init(forms, {edge: 'left'});
+})
---
+++ js/ui.js
@@ -0,0 +1,76 @@
+const posts = document.querySelector('.recipes')
+const side_menu = document.getElementById('side-menu')
+
+
+
+const not_logged_menu = `
+    <li><a class="subheader">TwitterAndreesen PWA</a></li>
+    <li><a href="/" class="waves-effect">
+      <i class="material-icons">home</i>Home</a>
+    </li>
+    <li><a href="/pages/about.html" class="waves-effect">
+      <i class="material-icons">account_circle</i>Login</a>
+    </li>
+    <li><div class="divider"></div></li>
+    <li><a href="/pages/contact.html" class="waves-effect">
+      <i class="material-icons">app_registration</i>Register</a>
+    </li>
+  `
+
+const logged_menu = `
+<li><a class="subheader">TwitterAndreesen PWA</a></li>
+<li><a href="/" class="waves-effect">
+  <i class="material-icons">home</i>Home</a>
+</li>
+<li><a href="/pages/about.html" class="waves-effect">
+  <i class="material-icons">account_circle</i>My Profile</a>
+</li>
+<li><div class="divider"></div></li>
+<li><a class="waves-effect" id="logout-btn">
+  <i class="material-icons">logout</i>logout</a>
+</li>
+`
+
+document.addEventListener('DOMContentLoaded', async function() {
+  // nav menu
+  const menus = document.querySelectorAll('.side-menu');
+  M.Sidenav.init(menus, {edge: 'right'});
+  // add recipe form
+  const forms = document.querySelectorAll('.side-form');
+  M.Sidenav.init(forms, {edge: 'left'});
+
+  var user = await localStorage.getItem('user')
+  if (user) {
+    side_menu.innerHTML += logged_menu
+    const logout_btn = document.getElementById('logout-btn')
+    logout_btn.addEventListener('click', () => {
+      console.log('penee')
+      localStorage.clear()
+      side_menu.innerHTML = not_logged_menu
+    })
+  } else {
+    side_menu.innerHTML += not_logged_menu
+  }
+  
+});
+
+// Render Post data
+const renderPost = (data, id) => {
+  const html = `
+  <div class="card-panel recipe white row" data-id="${id}">
+      <img src="https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg" alt="recipe thumb">
+      <div class="recipe-details">
+        <div class="recipe-title">${data.user}</div>
+        <div class="recipe-ingredients">${data.caption}</div>
+      </div>
+      <div class="recipe-delete">
+        <i class="material-icons" data-id="${id}">delete_outline</i>
+      </div>
+    </div>
+  `
+  posts.innerHTML += html
+} 
+
+
+
+
